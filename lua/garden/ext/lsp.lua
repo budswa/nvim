a4cf0a706b8188850847d942a132b753b97e984a
Eli Emanuel <eliemanuel@protonmail.com>
2021-11-05 12:30:44 +1300

    fixed capabilities

--- lua/garden/ext/lsp.lua
+++ lua/garden/ext/lsp.lua
@@ -14,17 +14,6 @@ vim.fn.sign_define('LspDiagnosticsSignInformation', { text = 'I', numhl = 'LspDi
 vim.fn.sign_define('LspDiagnosticsSignHint', { text = 'H', numhl = 'LspDiagnosticsDefaultHint' })
 
 local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities.textDocument.completion.completionItem.documentationFormat = { 'markdown' }
-capabilities.textDocument.completion.completionItem.snippetSupport = true
-capabilities.textDocument.completion.completionItem.preselectSupport = true
-capabilities.textDocument.completion.completionItem.insertReplaceSupport = true
-capabilities.textDocument.completion.completionItem.labelDetailsSupport = true
-capabilities.textDocument.completion.completionItem.deprecatedSupport = true
-capabilities.textDocument.completion.completionItem.commitCharactersSupport = true
-capabilities.textDocument.completion.completionItem.tagSupport = { valueSet = { 1 } }
-capabilities.textDocument.completion.completionItem.resolveSupport = {
-	properties = { 'documentation', 'detail', 'additionalTextEdits' },
-}
 capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
 
 vim.lsp.handlers['textDocument/publishDiagnostics'] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
