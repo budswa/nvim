edf682037008439641fe9441c7dcfc50902090a8
budswa <eliemanuel@protonmail.com>
2021-12-12 02:08:57 +1300

    removed unused util function

--- lua/garden/utils.lua
+++ lua/garden/utils.lua
@@ -1,3 +1,12 @@
+_G.reload = function()
+	for module, _ in pairs(package.loaded) do
+		if module:match('garden') then
+			package.loaded[module] = nil
+			require(module)
+		end
+	end
+end
+
 local M = {}
 
 function M.nvim_create_augroups(definitions)
@@ -10,32 +19,6 @@ function M.nvim_create_augroups(definitions)
 		end
 		vim.api.nvim_command('augroup END')
 	end
-end
-
-_G.reload = function()
-	for module, _ in pairs(package.loaded) do
-		if module:match('garden') then
-			package.loaded[module] = nil
-			require(module)
-		end
-	end
-end
-
-_G.profile = function(command, times)
-	times = times or 100
-	local args = {}
-	if type(cmd) == 'string' then
-		args = { cmd }
-		cmd = cmd
-	end
-	local start = vim.loop.hrtime()
-	for _ = 1, times, 1 do
-		local ok = pcall(command, unpack(args))
-		if not ok then
-			error('Command failed: ' .. tostring(ok) .. ' ' .. vim.inspect({ command = command, args = args }))
-		end
-	end
-	print(((vim.loop.hrtime() - start) / 1000000 / times) .. 'ms')
 end
 
 return M
